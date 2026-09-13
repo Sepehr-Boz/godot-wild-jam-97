c0c0eff0bc4691455d9b7c3c4782f6b68265fd53
Sepehr <seperbozorgy22@gmail.com>
2026-09-13 14:43:59 +0100

    refactor: move spawn move markers into own method.

--- scripts/characters_controller.gd
+++ scripts/characters_controller.gd
@@ -11,9 +11,6 @@ const CHARACTER_ATLAS_INDEX: Dictionary[Character, Vector2i] = {
 	Character.ARCHER: Vector2i(4, 9)
 }
 const MOVE_MARKER_ATLAS_INDEX: Vector2i = Vector2i(0, 5)
-const MOVE_DIRECTIONS: Array[Vector2i] = [
-	Vector2i(-1, 0), Vector2i(1, 0), Vector2i(0, -1), Vector2(0, 1)
-]
 
 var character_positions: Dictionary[Character, Vector2i] = {
 	Character.KNIGHT: Vector2i(8, 4),
@@ -28,17 +25,16 @@ var character_queue: Array[Character] = [
 var current_character: Character:
 	get:
 		return character_queue[0]
+var move_positions: Array[Vector2i]:
+	get:
+		return get_surrounding_cells(character_positions[current_character])
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	clear()
 	for type in Character.values():
 		set_cell(character_positions[type], 0, CHARACTER_ATLAS_INDEX[type])
-	for move_dir in MOVE_DIRECTIONS:
-		var target_position: Vector2i = character_positions[current_character] + move_dir
-		if not intersects_character(target_position):
-			set_cell(target_position, 0, MOVE_MARKER_ATLAS_INDEX)
-	
+	spawn_move_markers()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -77,10 +73,7 @@ func move_characters(dir: Vector2i) -> void:
 			character_positions[type] = prev_char_position
 		prev_char_position = char_position
 		set_cell(character_positions[type], 0, CHARACTER_ATLAS_INDEX[type])
-	for move_dir in MOVE_DIRECTIONS:
-		var target_position: Vector2i = character_positions[current_character] + move_dir
-		if not intersects_character(target_position):
-			set_cell(target_position, 0, MOVE_MARKER_ATLAS_INDEX)
+	spawn_move_markers()
 
 func switch_characters(direction: SwitchDirection) -> void:
 	clear()
@@ -104,8 +97,10 @@ func switch_characters(direction: SwitchDirection) -> void:
 			character_positions[character_queue[i]] = curr_position
 			curr_position = character_position
 			set_cell(character_positions[character_queue[i]], 0, CHARACTER_ATLAS_INDEX[character_queue[i]])
-	for move_dir in MOVE_DIRECTIONS:
-		var target_position: Vector2i = character_positions[current_character] + move_dir
+	spawn_move_markers()
+
+func spawn_move_markers() -> void:
+	for target_position in move_positions:
 		if not intersects_character(target_position):
 			set_cell(target_position, 0, MOVE_MARKER_ATLAS_INDEX)
 
