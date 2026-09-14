6ca0cf2b8e5e66b50982eef50e8d4c3e7b968572
Sepehr <seperbozorgy22@gmail.com>
2026-09-14 20:25:41 +0100

    feat: add enemy move animation, add enemy material.
    Character shader was extended to define the movement direction.
    Enemy tile material uses the character shader but with different properties.
    On action every enemy plays a short scale up-down animation.

--- scripts/enemy_controller.gd
+++ scripts/enemy_controller.gd
@@ -9,11 +9,16 @@ const ENEMY_SOURCE_INDEX: Dictionary[Enemy, int] = {
 
 @onready var _enemies_tilemap = $Enemies
 @export var enemy_paths: Dictionary[TileMapPath, Enemy] = {}
+@export var enemy_move_outline_color: Color = Color.BLACK
+var _enemy_material: ShaderMaterial = preload("res://materials/enemy_tile.tres")
+@onready var _rng: RandomNumberGenerator = RandomNumberGenerator.new()
+var _enemy_offsets: Dictionary[TileMapPath, float] = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for path: TileMapPath in enemy_paths.keys():
 		path.hide_path()
+		_enemy_offsets[path] = _rng.randf()
 	CharacterController.instance.increment_time.connect(
 		func ():
 			increment_time()
@@ -32,3 +37,16 @@ func increment_time() -> void:
 	for path: TileMapPath in enemy_paths:
 		path.increment_time()
 		_enemies_tilemap.set_cell(path.get_current_point(), ENEMY_SOURCE_INDEX[enemy_paths[path]], Vector2i.ZERO)
+		var shader: ShaderMaterial = _enemy_material.duplicate()
+		shader.set_shader_parameter("offset", _enemy_offsets[path])
+		_enemies_tilemap.get_cell_tile_data(path.get_current_point()).material = shader
+		_scale_enemy_animation(_enemies_tilemap.get_cell_tile_data(path.get_current_point()))
+
+func _scale_enemy_animation(tile: TileData) -> void:
+	var _animation = func (value: Vector2):
+		var shader: ShaderMaterial = tile.material
+		shader.set_shader_parameter("scale_addition", value)
+	
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_method(_animation, Vector2.ZERO, Vector2.ONE * 2, 0.1)
+	tween.tween_method(_animation, Vector2.ONE * 2, Vector2.ZERO, 0.1)
