553098daff79d205d9f4c2ab36f1ca981427558a
Sepehr <seperbozorgy22@gmail.com>
2026-09-19 16:56:58 +0100

    feat: display current action number on time slider.
    Moved game ui into its own scene.

--- scripts/time_slider.gd
+++ scripts/time_slider.gd
@@ -2,6 +2,7 @@ class_name TimeSlider
 extends HSlider
 
 @onready var _max_label: Label = $"../Label2"
+@onready var _value_label: Label = $"../Current Value"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +11,17 @@ func _ready() -> void:
 	max_value = GameManager.instance.max_actions_allowed
 	tick_count = max_value / 10
 	_max_label.text = str(GameManager.instance.max_actions_allowed)
-	GameManager.instance.increment_time.connect(func (x): value = x)
-	GameManager.instance.decrement_time.connect(func (x): value = x)
+	_value_label.text = "Time Step 0"
+	GameManager.instance.increment_time.connect(
+		func (x):
+			set_value_no_signal(x)
+			_value_label.text = "Time Step %d" % x
+	)
+	GameManager.instance.decrement_time.connect(
+		func (x):
+			set_value_no_signal(x)
+			_value_label.text = "Time Step %d" % x
+	)
 	value_changed.connect(_on_manual_change)
 
 func _on_manual_change(value: float) -> void:
